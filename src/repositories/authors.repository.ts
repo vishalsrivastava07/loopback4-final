18748854e2d82f75a7230644729e80ff909209b6
VishalSrivastava-007 <vishal.srivastava1@sourcefuse.com>
2025-03-21 10:35:51 +0530

    Added more APIs

--- src/repositories/authors.repository.ts
+++ src/repositories/authors.repository.ts
@@ -1,20 +1,3 @@
-// import {inject} from '@loopback/core';
-// import {SequelizeCrudRepository} from '@loopback/sequelize'
-// import {LoopbackintroDataSource} from '../datasources';
-// import {Authors, AuthorsRelations} from '../models';
-//
-// export class AuthorsRepository extends SequelizeCrudRepository<
-//   Authors,
-//   typeof Authors.prototype.authorId,
-//   AuthorsRelations
-// > {
-//   constructor(
-//     @inject('datasources.loopbackintro') dataSource: LoopbackintroDataSource,
-//   ) {
-//     super(Authors, dataSource);
-//   }
-// }
-
 import {inject, Getter} from '@loopback/core';
 import {repository, HasManyRepositoryFactory} from '@loopback/repository';
 import {SequelizeCrudRepository} from '@loopback/sequelize';
